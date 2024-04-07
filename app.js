0568556659458d2dd776081c5827176eee889b6e
riley <rearle1706@gmail.com>
2024-04-07 17:00:54 +0100

    cat-gallery

--- app.js
+++ app.js
@@ -17,3 +17,31 @@ const images = [
     alt: "eep",
   },
 ];
+
+function createThumbnails() {
+  images.forEach(function (image) {
+    const img = document.createElement("img");
+
+    img.src = image.url;
+    img.alt = image.alt;
+
+    thumbContainer.appendChild(img);
+
+    img.addEventListener("click", function () {
+      createBigImage(image);
+    });
+  });
+}
+
+function createBigImage(image) {
+  displayImage.innerHTML = "";
+  const bigImg = document.createElement("img");
+
+  bigImg.src = image.url;
+  bigImg.alt = image.alt;
+
+  displayImage.appendChild(bigImg);
+}
+
+createThumbnails();
+createBigImage(images[0]);
